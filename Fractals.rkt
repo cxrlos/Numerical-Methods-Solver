9c7f3eed60011fd2cf82f8732394e60d8569b3d6
Carlos García <A01025948@itesm.mx>
2020-06-13 21:28:41 -0500

    Last commit before merge to main

--- Fractals.rkt
+++ Fractals.rkt
@@ -3,15 +3,16 @@
 (require 2htdp/image)
 (require 2htdp/universe)
  
-; Constants
 (define WIDTH 300)
 (define HEIGHT 160)
 (define MT (empty-scene WIDTH HEIGHT))
  
-; next-size : Number -> Number
 (define (next-size s)
   (/ s 2))
 
-(define (test)
-  (circle 100 "solid" "red"))
+(define (circles x size screen)
+  (beside/align "center"
+                (circle size "outline" "dark blue")
+                (circle (next-size size) "outline" "dark blue")))
+      
 
