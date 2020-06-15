2be9426e65427688408b3231b91e174b0c443827
yannlelorier <yannllb97@gmail.com>
2020-06-14 21:42:06 -0500

    adding file io

--- Main.rkt
+++ Main.rkt
@@ -2,6 +2,39 @@
 
 (require "Functions.rkt")
 (require 2htdp/image)
+(require megaparsack megaparsack/text)
+
+(define si/p
+    (do (char/p #\s)
+        (char/p #\i)
+        (char/p #\,)
+        [x <- integer/p]
+        (pure x)))
+
+(define ko/p
+    (do (char/p #\k)
+        (char/p #\o)
+        (char/p #\,)
+        [x <- integer/p]
+        (pure x)))
+
+; (define req/p
+;     (do [x <- letter/p]
+;         (space/p)
+;         [y <- integer/p]
+;         (pure (+ x y))))
+
+(define (lines in)
+    (let loop
+        ([iter 0])
+        (define line (read-line in))
+        (if (eof-object? line)
+            result
+            ; Each line contains a number, so it must be converted
+            (cond 
+                [(parse-string si/p line) (loop (parse-string si/p line))]
+                [(parse-string ko/p line) (loop (parse-string ko/p line))]))))
+
 
 (define triangles (sierpinski-triangle 3))
 (define bg1 (rectangle (image-width triangles) (image-height triangles) "solid" "black"))
