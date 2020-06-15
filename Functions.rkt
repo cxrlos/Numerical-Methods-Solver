7d95b18f20e1a429b38ed6e068fa18be0f09b28f
Carlos García <A01025948@itesm.mx>
2020-06-15 00:59:36 -0500

    Finished and documented Sierpinski function, missing own development of the Koch function

--- Functions.rkt
+++ Functions.rkt
@@ -1,21 +1,48 @@
+#|------------------------------------------------- Functions.rkt ---------
+    |   Purpose: Contains the algorithms for the functions called by the
+    |       Main.rkt script. 
+    |
+    |   Functions:
+    |       sierpinski - Receives the number of levels desired, outputs the
+    |           respective Sierpinski fractal.
+    |       koch - Receives the number of levels desired, outputs the
+    |           respective Koch fractal.
+    |
+    |   Developers:  
+    |       Carlos García - https://github.com/cxrlos
+    |       Yann Le Lorier - https://github.com/yannlelorier
+    |
+    *--------------------------------------------------------------------|#
+
 #lang racket
-(provide sierpinski-triangle koch-curve)
-(require 2htdp/image)
-(define (sierpinski-triangle l)
-  (if (<= l 4)
-      (triangle l "outline" "black")
-      (local [(define sub (sierpinski-triangle (/ l 2)))]
-        (overlay (triangle l "outline" "black")
-                 (above sub
-                        (beside sub sub))))))
+(provide sierpinski koch) ; Sends the functions to Main.rkt
+(require 2htdp/image) ; Package for images
 
+; --- NOTE ---
+; Some functions were not seen in class but were found in the documentation
+; https://docs.racket-lang.org/teachpack/2htdpuniverse.html
+; https://docs.racket-lang.org/teachpack/2htdpimage-guide.html
 
+(define (sierpinski levels)
+  (if (not (zero? levels))
+      
+      ; Function used in 2htdp/image scenarios, adapted it to our reqs.
+      (local
+        
+        ; Defines the next-level that the algorithm should process
+        ((define next-level (sierpinski (- levels 1))))
+        
+        ; Places the triangles above and next to a reference triangle
+        (above next-level (beside next-level next-level)))
 
-(define (koch-curve n)
+      ; Base case, draws a triangle
+      (triangle 20 "outline" "black")))
+
+(define (koch n)
   (cond
     [(zero? n) (line 20 0 (pen "black" 8 "solid" "butt" "miter"))]
     [else
-     (local [(define smaller (koch-curve (- n 1)))]
+     (local [(define smaller (koch (- n 1)))]
        (beside/align "bottom"
                      smaller
                      (rotate 60 smaller)
--- Main.rkt
+++ Main.rkt
@@ -3,10 +3,21 @@
 (require "Functions.rkt")
 (require 2htdp/image)
 
-(define triangles (sierpinski-triangle 750))
+(fprintf (current-output-port)"  ____________________________________________  \n")
+(fprintf (current-output-port)"|                                              |\n")
+(fprintf (current-output-port)"|                                              |\n")
+(fprintf (current-output-port)"| Welcome! The functions will process shortly. |\n")
+(fprintf (current-output-port)"|                                              |\n")
+(fprintf (current-output-port)"  ____________________________________________  \n")
+(fprintf (current-output-port)"\n")
+(writeln 'Processing_Sierpinski...)
+(define triangles (sierpinski 7))
 (define bg1 (rectangle (image-width triangles) (image-height triangles) "solid" "white"))
 (save-svg-image (overlay/align "center" "bottom" triangles bg1) "sierpinski.svg")
 
-(define curve (koch-curve 5))
+(writeln 'Processing_Koch...)
+(define curve (koch 3))
 (define bg (rectangle (image-width curve) (image-height curve) "solid" "white"))
 (save-svg-image (overlay/align "center" "bottom" curve bg) "koch.svg")
+
+(fprintf (current-output-port)"\nAll done! The images will be contained where the scripts are located.")
